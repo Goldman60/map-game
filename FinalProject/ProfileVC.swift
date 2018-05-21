528a50d1f282ae433ad1ff1f4bb9da7d2ffc7070
AJ Fite <me@ajfite.com>
2018-05-20 22:24:15 -0700

    Lots of storyboard work and authentication work

---
+++ FinalProject/ProfileVC.swift
@@ -0,0 +1,70 @@
+//
+//  SecondViewController.swift
+//  FinalProject
+//
+//  Created by AJ Fite on 5/19/18.
+//  Copyright © 2018 AJ Fite. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import GoogleSignIn
+
+//Login and other related tasks accomplished here
+class ProfileVC: UIViewController, GIDSignInUIDelegate {
+    
+    @IBOutlet weak var googleSignInButton: GIDSignInButton!
+    @IBOutlet weak var logoutButton: UIButton!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var userPlaceLabel: UILabel!
+    @IBOutlet weak var userImageLabel: UIImageView!
+    @IBOutlet weak var userEmailLabel: UILabel!
+    @IBOutlet weak var editUserLabel: UIButton!
+    @IBOutlet weak var userRealNameLabel: UILabel!
+    
+    @IBAction func signInButtonPressed(_ sender: GIDSignInButton) {
+         GIDSignIn.sharedInstance().signIn()
+    }
+    
+    @IBAction func logoutButtonPressed(_ sender: UIButton) {
+        let firebaseAuth = Auth.auth()
+        do {
+            try firebaseAuth.signOut()
+        } catch let signOutError as NSError {
+            print ("Error signing out: %@", signOutError)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if let user = Auth.auth().currentUser {
+            googleSignInButton.isHidden = true
+            logoutButton.isHidden = false
+            editUserLabel.isHidden = false
+            
+            userEmailLabel.text = user.email
+            userRealNameLabel.text = user.displayName
+        }
+        else {
+            googleSignInButton.isHidden = false
+            logoutButton.isHidden = true
+            editUserLabel.isHidden = true
+            
+            userEmailLabel.text = "SIGNED OUT"
+            userRealNameLabel.text = "SIGNED OUT"
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        GIDSignIn.sharedInstance().uiDelegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
