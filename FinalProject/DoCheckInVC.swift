318bb737bf824e11f3d68e4346e59200169eb324
AJ Fite <me@ajfite.com>
2018-05-29 18:35:06 -0700

    Pod update and work on photos

--- FinalProject/DoCheckInVC.swift
+++ FinalProject/DoCheckInVC.swift
@@ -7,18 +7,91 @@
 //
 
 import UIKit
+import Photos
+import Firebase
 
-class DoCheckInVC: UIViewController {
+class DoCheckInVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var databaseRef : DatabaseReference!
+    var imagesRef  :  StorageReference!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        databaseRef = Database.database().reference().child("photoInfo")
+        imagesRef = Storage.storage().reference().child("images")
 
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBOutlet weak var checkInPhoto: UIImageView!
+    
+    @IBAction func completeCheckIn(_ sender: Any) {
+        
+    
+        
+        let photoInfo = PhotoInfo(title: "Test Image")
+        
+        let newPhotoInfoRef = databaseRef.child("Test Image")
+        newPhotoInfoRef.setValue(photoInfo.toAny())
+        
+        let imageData = UIImageJPEGRepresentation(checkInPhoto.image!, 1.0)
+        let metadata = StorageMetadata()
+        metadata.contentType = "image/jpeg"
+        let imagePath = "Test Image" + ".jpeg"
+        print(imagePath)
+        
+        self.imagesRef.child(imagePath).putData(imageData!, metadata: metadata) { (metadata, error) in
+            
+            if let error = error {
+                print("Error uploading: \(error) for image: \(imagePath)")
+            }
+            else {
+                if let url = metadata?.downloadURL()?.absoluteString{
+                    print("Image uploaded at \(url)")
+                    // version without callback - can use either one
+                    //                    newPhotoInfoRef.updateChildValues(["photoKey" : url])
+                    newPhotoInfoRef.updateChildValues(["photoKey" : url])
+                    { error, databaseRef in
+                        if let error = error {
+                            print("Error updating image URL: \(error)")
+                        }
+                        else {
+                            print("Image URL successfully updated.")
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    @IBAction func takePhoto(_ sender: UIButton) {
+        let picker = UIImagePickerController()
+        
+        picker.delegate = self
+        picker.sourceType = UIImagePickerController.isSourceTypeAvailable(.camera) ? .camera : .photoLibrary
+        
+        present(picker, animated: true, completion: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - UIImagePickerControllerDelegate methods
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        guard let image = info[UIImagePickerControllerOriginalImage] as? UIImage else { return }
+        
+        checkInPhoto.image = image
+        
+        picker.dismiss(animated: true, completion:nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion:nil)
     }
     
 
--- Pods/Target Support Files/Pods-FinalProject/Pods-FinalProject-frameworks.sh
+++ Pods/Target Support Files/Pods-FinalProject/Pods-FinalProject-frameworks.sh
@@ -145,7 +145,6 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/GTMOAuth2/GTMOAuth2.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GTMSessionFetcher/GTMSessionFetcher.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/GeoFire/GeoFire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GoogleToolboxForMac/GoogleToolboxForMac.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/leveldb-library/leveldb.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
@@ -153,7 +152,6 @@ fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/GTMOAuth2/GTMOAuth2.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GTMSessionFetcher/GTMSessionFetcher.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/GeoFire/GeoFire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GoogleToolboxForMac/GoogleToolboxForMac.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/leveldb-library/leveldb.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
